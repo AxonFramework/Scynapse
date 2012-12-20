8f3657f2f4c282a50ce6e4210b2e4a8c788abd24
Maxim Fedorov <familiar.hedgehog@gmail.com>
2012-12-20 18:19:31 +0200

    lowercase list alias

--- src/main/scala/com/thenewmotion/scynapse/serialization/xml/XStreamSerializer.scala
+++ src/main/scala/com/thenewmotion/scynapse/serialization/xml/XStreamSerializer.scala
@@ -3,6 +3,6 @@ package com.thenewmotion.scynapse.serialization.xml
 import org.axonframework.serializer.xml.{XStreamSerializer => AxonXStreamSerializer}
 
 class XStreamSerializer extends AxonXStreamSerializer {
-  getXStream.alias("List", classOf[::[_]])
+  getXStream.alias("list", classOf[::[_]])
   getXStream.registerConverter(new ListXStreamConverter(getXStream.getMapper))
 }
