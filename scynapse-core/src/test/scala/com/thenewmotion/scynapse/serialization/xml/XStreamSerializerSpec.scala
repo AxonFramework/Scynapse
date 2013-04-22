82dceacb2c0739beed7602af105848236234107a
Igor Shymko <igor.shimko@gmail.com>
2013-04-22 12:15:55 +0300

    XStreamSerializerSpec shifted to scalatest

--- scynapse-core/src/test/scala/com/thenewmotion/scynapse/serialization/xml/XStreamSerializerSpec.scala
+++ scynapse-core/src/test/scala/com/thenewmotion/scynapse/serialization/xml/XStreamSerializerSpec.scala
@@ -1,30 +1,9 @@
 package com.thenewmotion.scynapse.serialization.xml
 
-import org.specs2.Specification
-
-class XStreamSerializerSpec extends Specification { def is =
-
-  "XStreamSerializer supports"                              ^
-    "Immutable list serialization"                          ! sut().serialize ^
-    end
-
-  case class sut() {
-    val serializer = new XStreamSerializer
-    val testEvent = ListTestEvent(List("abc", "bcd", "cde"))
-
-    def serialize = {
-      val ser = serializer.serialize(testEvent, classOf[Array[Byte]])
-      (serializer.deserialize(ser): ListTestEvent) mustEqual testEvent
-    }
-  }
-
-  case class ListTestEvent(elements: List[String])
-
-}
-
-/*
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.MustMatchers
+
+case class ListTestEvent(elements: List[String])
 
 class XStreamSerializerSpec extends FlatSpec with MustMatchers {
 
@@ -36,8 +15,4 @@ class XStreamSerializerSpec extends FlatSpec with MustMatchers {
     (serializer.deserialize(ser): ListTestEvent) must equal(testEvent)
   }
 
-  case class ListTestEvent(elements: List[String])
-
 }
-
-*/
